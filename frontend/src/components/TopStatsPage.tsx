bff48ecf6327aaf5a7606799df01763f53e54a6c
tnguyen0000 <tomnguyentn246@gmail.com>
2025-01-24 17:43:57 +1100

    added css & type selection for topstats page

--- frontend/src/components/TopStatsPage.tsx
+++ frontend/src/components/TopStatsPage.tsx
@@ -1,6 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import './styles/userstatspage.css'
 
 const TopStatsPage = () => {
+  const [checked, setChecked] = useState('long_term');
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     let type = urlParams.get('type');
@@ -14,12 +17,49 @@ const TopStatsPage = () => {
       default:
         console.log("TODO!: ERROR CHECL")
     }
-  });
+  }, []);
+
+  useEffect(() => {
+    const radio = document.getElementsByName('category');
+    for (const input of radio) {
+      input.addEventListener('change', () => {
+        setChecked((input as HTMLInputElement).value);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(checked)
+  }, [checked]);
 
     return (
-      <>
-          TODO!
-      </>
+      <div id="statspage-container">
+          <div id="category-container">
+            <label className="category-label" htmlFor="last-year">
+              <input type="radio" id="last-year" name="category" value={'long_term'} 
+                defaultChecked
+              />
+              <span className="category-select-btn">
+                Last Year
+              </span>
+            </label>
+            <label className="category-label" htmlFor="last-6-months">
+              <input type="radio" id="last-6-months" name="category" value={'medium_term'} />
+              <span className="category-select-btn">
+                Last 6 Months
+              </span>
+            </label>
+            <label className="category-label" htmlFor="last-4-wks">
+              <input type="radio" id="last-4-wks" name="category" value={'short_term'} />
+              <span className="category-select-btn">
+                Last 4 Weeks
+              </span>
+            </label>
+          </div>
+          <div id="items-container">
+            Songs
+          </div>
+      </div>
     );
   };
   
